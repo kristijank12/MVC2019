47b166971348a15716019c546c052590539c715b
kikijela12@gmail.com <kikijela12@gmail.com>
2019-03-28 18:32:30 +0100

    Vjezba 2.1.1

---
+++ Matematicki izraz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Matematicki_izraz.MvcApplication" Language="C#" %>
---
+++ hello world/Controllers/KreiranjekontroleraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hello_world.Controllers
+{
+    public class KreiranjekontroleraController : Controller
+    {
+        // GET: Kreiranjekontrolera
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
