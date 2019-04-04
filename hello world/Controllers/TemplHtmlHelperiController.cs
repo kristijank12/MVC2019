998325f444e167c7a8d323fc5f3216208aba6361
kikijela12@gmail.com <kikijela12@gmail.com>
2019-04-04 18:03:58 +0200

    LayOut

---
+++ hello world/Controllers/TemplHtmlHelperiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hello_world.Models;
+
+namespace hello_world.Controllers
+{
+    public class TemplHtmlHelperiController : Controller
+    {
+        // GET: TemplHtmlHelperi
+        public ViewResult HtmlEditorView()
+        {
+            return View(new OsobaTempt());
+        }
+
+        [HttpPost]
+        public ViewResult HtmlEditorView(OsobaTempt osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
