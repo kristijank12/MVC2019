898d112b23dd05d381d9375fb5f76f126765d559
kikijela12@gmail.com <kikijela12@gmail.com>
2019-04-08 17:44:25 +0200

    Changes

---
+++ Validacije/Controllers/ValidacijeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validacije.Models;
+
+namespace Validacije.Controllers
+{
+    public class ValidacijeController : Controller
+    {
+        // GET: Validacije
+        public ViewResult Izdavanjeracuna()
+        {
+            return View(new Racun() {Datum = DateTime.Now, BrojRacuna = "/");
+        }
+    }
+}
---
+++ ado_net_spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ado_net_spajanje_na_bazu.Controllers
+{
+    public class SqlConnectionObjectController : Controller
+    {
+        // GET: SqlConnectionObject
+        public ActionResult Index()
+        {
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+            return View();
+        }
+    }
+}
